da22453c9e2f7003a8845f872a815436ea6576a3
Bas667 <161934474+Bas667@users.noreply.github.com>
2024-04-22 19:31:49 +0200

    Erste Version - muss noch aufgeräumt werden

---
+++ assets/coin.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="coin" tilewidth="70" tileheight="70" tilecount="2" columns="2">
+ <image source="coinGold__.png" width="140" height="70"/>
+</tileset>
---
+++ assets/coinGold.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="coinGold" tilewidth="60" tileheight="60" tilecount="1" columns="1">
+ <image source="coinGold.png" width="70" height="70"/>
+</tileset>
