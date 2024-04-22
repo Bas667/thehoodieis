280e96b687328af4c908e2dcf5180c77d02f8c43
Bas667 <161934474+Bas667@users.noreply.github.com>
2024-04-22 19:34:02 +0200

    Ein paar ungenutzte Assets gelöscht

--- assets/Project Mute Tileset V1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Project Mute Tileset V1" tilewidth="32" tileheight="32" tilecount="48" columns="8">
- <image source="../../City Tileset's/City Tileset's/Project Mute Tileset V1.png" width="256" height="208"/>
-</tileset>
--- assets/hoodie.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="hoodie" tilewidth="60" tileheight="60" tilecount="64" columns="8">
- <image source="../../../../StableDiffusion/ComfyUI/ComfyUI/output/AnimateDiff_00082.png" width="512" height="512"/>
-</tileset>
